1e1d882d5e4d8b89e6a358b41c3a8048a04e1de1
Chrisemprayil <billchristinemprayil@gmail.com>
2024-05-15 11:44:32 +0530

    updated

---
+++ src/main/java/com/chris/SpringBootRest/controller/UserRegisterController.java
@@ -0,0 +1,19 @@
+package com.chris.SpringBootRest.controller;
+
+import com.chris.SpringBootRest.Model.users;
+import com.chris.SpringBootRest.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRegisterController {
+@Autowired
+    private UserService service;
+@PostMapping("register")
+    public users register(@RequestBody users user){
+        return service.saveUser(user);
+
+    }
+}
